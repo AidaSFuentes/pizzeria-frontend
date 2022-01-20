052b00f7eed267bc3b182b348b5d043ec33041e2
AidaSFuentes <aidadesarrolloweb@gmail.com>
2022-01-20 17:25:05 +0100

    update enviroment.prod.ts and enviroment.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
+  ERROR_LEVEL: 1,
   securityApiURL: 'http://localhost:4321/',
+  apiURL: '/api/',
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
+  ERROR_LEVEL: 99,
   securityApiURL: 'http://localhost:4321/',
+  apiURL: 'http://localhost:4321/api/',
 };
 
 /*
