62b24eebc86190e445a4f61fcbcc3cea9d57d316
Fer3D <ferbg9@gmail.com>
2022-01-20 15:32:51 +0100

    del home component and add icons to navbar

--- pizzeria/src/app/main/main.module.ts
+++ pizzeria/src/app/main/main.module.ts
@@ -3,22 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-
-
 
 @NgModule({
   declarations: [
     HeaderComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
   ],
   exports: [
     HeaderComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
   ],
   imports: [
     CommonModule
