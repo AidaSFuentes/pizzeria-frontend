91c7ebc6839cfcd6acdb0924bf6180ddf778375e
Fer3D <ferbg9@gmail.com>
2022-01-20 15:43:04 +0100

    created slider component with a static image

--- pizzeria/src/app/main/main.module.ts
+++ pizzeria/src/app/main/main.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     NavbarComponent,
     FooterComponent,
+    SliderComponent,
   ],
   exports: [
     HeaderComponent,
     NavbarComponent,
     FooterComponent,
+    SliderComponent,
   ],
   imports: [
     CommonModule
