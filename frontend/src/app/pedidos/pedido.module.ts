f0ff3dac21bfd9513b23414fc01ea562ce2de17d
AidaSFuentes <aidadesarrolloweb@gmail.com>
2022-01-20 22:34:06 +0100

    Create and Update pedidos with all components, index and module.ts

---
+++ frontend/src/app/pedidos/pedido.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PedidoComponent} from './pedido/pedido.component';
+
+
+
+@NgModule({
+  declarations: [
+    PedidoComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PedidoModule { }
