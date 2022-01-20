76f60560088c0fa55a14a395c0af59a257a568be
AidaSFuentes <aidadesarrolloweb@gmail.com>
2022-01-20 22:28:15 +0100

    Create and Update Usuarios

--- frontend/src/app/usuarios/usuarios.module.ts
+++ frontend/src/app/usuarios/usuarios.module.ts
@@ -1,12 +1,21 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { UsuarioComponent } from './usuario/usuario.component';
 
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: UsuarioComponent },
+];
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UsuarioComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule, RouterModule.forChild(routes),
   ]
 })
-export class UsuariosModule { }
+export class UsuarioModule { }
