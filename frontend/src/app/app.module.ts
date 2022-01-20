da5621254f2c667a60fcea25f3986b5f15825790
AidaSFuentes <aidadesarrolloweb@gmail.com>
2022-01-20 22:35:39 +0100

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,24 +2,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MyCoreModule } from 'src/lib/my-core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CartaModule } from './carta/carta.module';
+import { CatalogoModule } from './catalogo/catalogo.module';
+import { CommonServicesModule } from './common-services/common-services.module';
+import { CommonComponentsModule } from './common.component';
+import { IngredienteModule } from './ingredientes/ingrediente.module';
 import { MainModule } from './main/main.module';
+import { PedidoModule } from './pedidos/pedido.module';
 import { SecurityModule } from './security';
-import { ShowErrorsMessagesComponent } from './common.component/show-errors-messages/show-errors-messages.component';
-import { IngredienteModule } from './ingredientes';
-import { PizzasModule } from './pizzas/pizzas.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShowErrorsMessagesComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule,
-    AppRoutingModule, CartaModule, MainModule, SecurityModule, IngredienteModule, PizzasModule
+   HttpClientModule, FormsModule, BrowserModule, AppRoutingModule, MainModule, CommonComponentsModule,
+   CommonServicesModule, SecurityModule, CatalogoModule, PedidoModule, MyCoreModule, IngredienteModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
