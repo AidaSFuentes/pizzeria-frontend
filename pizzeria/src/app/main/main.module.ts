ad4d8af7a528b7f6370169901aef745d4156d443
Fer3D <ferbg9@gmail.com>
2022-01-20 13:34:10 +0100

    Added components: Header, navbar, footer

--- pizzeria/src/app/main/main.module.ts
+++ pizzeria/src/app/main/main.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
   ]
