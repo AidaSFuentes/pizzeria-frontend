13e47cbe5ce5f54b223791429962175ba364361b
alejandrocheca <alexcheca95@gmail.com>
2022-01-20 15:34:35 +0100

    Update pedidos.module.ts
    Actualización archivo pedidos.module.ts

--- pizzeria/src/app/pedidos/pedidos.module.ts
+++ pizzeria/src/app/pedidos/pedidos.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LibModule } from 'src/lib/lib.module';
+import { IngredientesModule } from '../ingredientes/ingredientes.module';
+
 
 
 
@@ -9,4 +12,31 @@ import { CommonModule } from '@angular/common';
     CommonModule
   ]
 })
-export class PedidosModule { }
+export class PedidosModule {
+  id: number | null = null;
+  nombre: string | null = null;
+  descripcion: string | null = null;
+  precio: number | null = null;
+
+ }
+
+
+  export class FormulariPedido implements OnInit{
+
+    elemento: PedidosModule = new FormularioPedido();
+    isAdd: boolean = true;
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    add(): void {
+      this.elemento = new PedidosModule();
+      this.isAdd = true;
+    }
+      cancel(): void {
+
+      }
+
+}
