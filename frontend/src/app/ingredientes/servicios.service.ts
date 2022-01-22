74f7946054ea309d018675779be389194d0d95ff
AidaSFuentes <aidadesarrolloweb@gmail.com>
2022-01-22 20:43:06 +0100

    Export class ingrediente import auth, router and navigation

--- frontend/src/app/ingredientes/servicios.service.ts
+++ frontend/src/app/ingredientes/servicios.service.ts
@@ -2,12 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpContext } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { AUTH_REQUIRED } from '../security';
-import { NotificationService } from '../common-services';
+import { AuthService, AUTH_REQUIRED } from '../security';
+import { NavigationService, NotificationService } from '../common-services';
 import { LoggerService } from 'src/lib/my-core';
 import { RESTDAOServiceBase, ViewModelServiceBase } from '../base';
 import { ModoCRUD } from '../base';
+import { Router } from '@angular/router';
 
+// export class Ingrediente {
+//   id: number = 0;
+//   nombre: string | null = null;
+//   tipo: string | null = null;
+//   precio: number = 0;
+// }
 
 @Injectable({
   providedIn: 'root',
@@ -15,7 +22,8 @@ import { ModoCRUD } from '../base';
 export class IngredientesDAOService extends RESTDAOServiceBase<any, any> {
   constructor(http: HttpClient) {
     super(http, 'ingredientes', {
-      context: new HttpContext().set(AUTH_REQUIRED, true),
+      //context: new HttpContext().set(AUTH_REQUIRED, true),
+      context: new HttpContext(),
     });
   }
 }
@@ -27,8 +35,12 @@ export class IngredientesViewModelService extends ViewModelServiceBase<any, any>
   constructor(
     notify: NotificationService,
     out: LoggerService,
-    dao: IngredientesDAOService
+    dao: IngredientesDAOService,
+    auth: AuthService,
+    router: Router,
+    navigation: NavigationService,
   ) {
-    super(notify, out, dao);
+    super(notify, out, dao, auth, router, navigation);
   }
 }
+
