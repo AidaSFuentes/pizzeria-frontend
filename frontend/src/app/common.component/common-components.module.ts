f2cc220aab15484b9a193ebcb4e677fdcfb333ed
AidaSFuentes <aidadesarrolloweb@gmail.com>
2022-01-24 10:36:17 +0100

    Updated common.component.module.ts and ingredientes form and module

--- frontend/src/app/common.component/common-components.module.ts
+++ frontend/src/app/common.component/common-components.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ShowErrorsMessagesComponent } from './show-errors-messages/show-errors-messages.component';
 
 @NgModule({
-  declarations: [],
-  exports: [],
+  declarations: [
+    ShowErrorsMessagesComponent,
+  ],
+  exports: [
+    ShowErrorsMessagesComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+
   ]
 })
 export class CommonComponentsModule { }
